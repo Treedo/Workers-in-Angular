b568664a93c2a553ed5bff600899349494ea5525
Treedo <s@treedo.name>
2021-12-12 15:44:05 +0200

    components events

--- src/app/poster/poster.component.ts
+++ src/app/poster/poster.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, OnChanges, DoCheck, Input, Output, SimpleChanges } from '@angular/core';
 
 
 @Component({
@@ -6,13 +6,26 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './poster.component.html',
   styleUrls: ['./poster.component.css']
 })
-export class PosterComponent {
+export class PosterComponent implements OnChanges, DoCheck {
 
   @Input() name: string = "";
   bday: Date = new Date() ;
 
   @Output() addWor = new EventEmitter();
   @Output() nameChange = new EventEmitter();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    for (let propName in changes) {
+      if(propName == "name" && !changes[propName].firstChange) {
+        console.log(changes[propName].currentValue);
+      }
+      
+    }
+  }
+
+  ngDoCheck(): void {
+    console.log("Something changed in 'Poster' component...");
+  }
 
   addWorker(name: string, bday: Date): void {
     this.addWor.emit({name: name, bday: bday}); 
