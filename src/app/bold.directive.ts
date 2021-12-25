43331c8b9a7210ce16eb4896c52316e735dab68a
Treedo <s@treedo.name>
2021-12-25 18:10:12 +0200

    Directive - dynamic change html (Host Listeners

--- src/app/bold.directive.ts
+++ src/app/bold.directive.ts
@@ -1,12 +1,40 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appBold]'
+  selector: '[appBold]',
+  host: {
+    '(mouseleave)': 'onMouseLeave()'
+  }
 })
 export class BoldDirective {
-
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
-    this.renderer.setStyle(this.elRef.nativeElement, "font-weight", "bold") ;
-   }
+    this.renderer.setStyle(this.elRef.nativeElement, 'font-weight', 'bold');
+  }
+
+  @Input()
+  itIsTitle: boolean = false;
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    if (this.itIsTitle) {
+      this.renderer.setStyle(this.elRef.nativeElement, 'color', 'red');
+    }
+  }
+
+  onMouseLeave() {
+    if (this.itIsTitle) {
+      this.renderer.removeStyle(this.elRef.nativeElement, 'color');
+    }
+  }
+
+  @HostBinding("style.border")
+  get getFontWeight(){
+    return "1px solid";
+  }
+
+  @HostBinding("style.cursor")
+  get getCursor(){
+    return "pointer";
+  }
 
 }
