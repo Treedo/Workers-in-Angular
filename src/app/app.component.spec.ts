d426d0a7a154dd56b1ec3d1674b8d0e3a2887d14
Treedo <s@treedo.name>
2021-12-26 01:22:51 +0200

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('workers app is running!');
+    expect(compiled.textContent).toContain('Список workers');
   });
 });
