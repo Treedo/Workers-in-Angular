0fb2b3201c3300ef3b2ac8fcfff8535aaefbc5a2
Treedo <s@treedo.name>
2022-01-01 00:35:20 +0200

    fix test Worker

--- src/app/models/workers/worker.spec.ts
+++ src/app/models/workers/worker.spec.ts
@@ -1,7 +1,8 @@
+import { DatePipe } from '@angular/common';
 import { Worker } from './worker';
 
 describe('Worker', () => {
   it('should create an instance', () => {
-    expect(new Worker()).toBeTruthy();
+    expect(new Worker(1, "test", new Date(2000, 1, 1), true)).toBeTruthy();
   });
-});
+})
